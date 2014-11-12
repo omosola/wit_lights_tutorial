77e57917251dce5c0b3234bed3dec45b71fbbd6a
Omosola <omosolao@stanford.edu>
2014-11-11 19:43:52 -0800

    made changes described on github page https://github.com/omosola/wit_lights_tutorial

---
+++ witty_lights/public/javascripts/scripts.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+	var mic = new Wit.Microphone(document.getElementById("microphone"));
+	var info = function (msg) {
+	    document.getElementById("info").innerHTML = msg;
+	};
+	mic.onready = function () {
+	    info("Microphone is ready to record");
+	};
+	mic.onaudiostart = function () {
+	    info("Recording started");
+	};
+	mic.onaudioend = function () {
+	    info("Recording stopped, processing started");
+	};
+	mic.onerror = function (err) {
+	    info("Error: " + err);
+	};
+	mic.onresult = function (intent, entities) {
+	    console.log(intent, entities);
+	    var result = concatKeyValue("intent", intent);
+
+	    for (var k in entities) {
+		var e = entities[k];
+
+		if (!(e instanceof Array)) {
+		    result += concatKeyValue(k, e.value);
+		} else {
+		    for (var i = 0; i < e.length; i++) {
+			result += concatKeyValue(k, e[i].value);
+		    }
+		}
+
+		document.getElementById("result").innerHTML = result;
+	    }
+	};
+
+	mic.connect("INSERT WIT CLIENT KEY HERE");
+
+	function concatKeyValue (k, v) {
+	    if (typeof v !== "string") {
+		v = JSON.stringify(v);
+	    }
+	    return k + "=" + v + "\n";
+	}
+    });
